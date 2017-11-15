ea815a32e53c201baf5b92731dc6b7c619033549
ItielBorja <33648137+ItielBorja@users.noreply.github.com>
2017-11-15 00:02:56 -0600

    Update Personaje.java

--- Personaje.java
+++ Personaje.java
@@ -1,1 +1,100 @@
-# RPG_POO
+public class Personaje{
+
+	private String 	nombre;
+	private int 	nivel;
+	private int 	hp;
+	private int 	ataque;
+	private int 	defensa;
+
+//constructor
+
+	public Personaje(){
+
+
+
+	}
+
+//set nombre 
+
+	public void setNombre(String nombre){
+
+		this.nombre= nombre;
+
+	}
+
+//get nombre
+
+	public String getNombre(){
+
+		return nombre;
+
+	}
+
+//set nivel
+
+	public void setNivel(int nivel){
+
+		
+		this.nivel = nivel;
+
+	}
+
+//get nivel
+
+	public int getNivel(){
+
+		return nivel;
+
+	}
+
+//set HP
+
+	public void setHp(int hp){
+
+		this.hp= hp;
+
+	}
+
+//get HP
+
+	public int getHp(){
+
+		return hp;
+
+	}
+
+//set ataque
+
+	public void setAtaque(int ataque){
+
+		this.ataque= ataque;
+
+	}
+
+//get ataque
+
+	public int getAtaque(){
+
+		return ataque;
+
+	}
+
+//set defensa
+
+	public void setDefensa(int defensa){
+
+		this.defensa= defensa;
+
+	}
+
+//get defensa
+
+	public int getDefensa(){
+
+		return defensa;
+
+	}
+
+
+
+}// clase
