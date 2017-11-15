be8fc0af3d11da3c0ddc48c7d05374544273d879
ItielBorja <33648137+ItielBorja@users.noreply.github.com>
2017-11-15 00:08:06 -0600

    Add files via upload

---
+++ Pociones.java
@@ -0,0 +1,50 @@
+public class Pociones extends Item{
+	
+	private int especialidad;
+	private int aumento;
+
+//constructor
+
+	public Pociones(){
+
+
+
+	}
+
+//set aumento
+
+	public void setAumento(int aumento){
+
+		
+		this.aumento = aumento;
+
+	}
+
+//get aumento
+
+	public int getAumento(){
+
+		return aumento;
+
+	}
+
+//set especialidad
+
+	public void setEspecialidad(int especialidad){
+
+		
+		this.especialidad = especialidad;
+
+	}
+
+//get especialidad
+
+	public int getEspecialidad(){
+
+		return especialidad;
+
+	}
+
+
+
+}//clase
