1cb1f7b7b5199fe6ef4fb001d001505ef93027ed
ItielBorja <33648137+ItielBorja@users.noreply.github.com>
2017-11-15 00:05:46 -0600

    Add files via upload

---
+++ Arma.java
@@ -0,0 +1,67 @@
+public class Arma extends Item{
+	
+	private int nivel;
+	private int daño;
+	private int rango;
+
+//constructor
+
+	public Arma(){
+
+
+		
+	}
+
+//set nivel
+
+	public void setNivel(int nivel){
+
+		
+		this.nivel = nivel;
+
+	}
+
+//get nivel
+
+	public int getNivel(){
+
+		return nivel;
+
+	}
+
+//set arma
+
+	public void setArma(int arma){
+
+		
+		this.arma = arma;
+
+	}
+
+//get arma
+
+	public int getArma(){
+
+		return arma;
+
+	}
+
+//set rango
+
+	public void setRango(int rango){
+
+		
+		this.rango = rango;
+
+	}
+
+//get rango
+
+	public int getRango(){
+
+		return rango;
+
+	}
+
+
+}// clase 
