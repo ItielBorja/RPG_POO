3fbaf593200107509d4b6457a3532a18caa9796a
ItielBorja <33648137+ItielBorja@users.noreply.github.com>
2017-11-15 00:07:17 -0600

    Add files via upload

---
+++ Armadura.java
@@ -0,0 +1,67 @@
+public class Armadura extends Item{
+	
+	private int nivel;
+	private int defensa;
+	private int especialidad;
+
+//constructor
+
+	public Armadura(){
+
+
+
+	}
+
+//set nivel
+
+	public void setNivel(int nivel){
+
+		
+		this.nivel = nivel;
+
+	}
+
+//get nivel
+
+	public int getNivel(){
+
+		return nivel;
+
+	}
+
+//set defensa
+
+	public void setDefensa(int defensa){
+
+		
+		this.defensa = defensa;
+
+	}
+
+//get defensa
+
+	public int getDefensa(){
+
+		return defensa;
+
+	}
+
+//set especialidad
+
+	public void setEspecialidad(int especialidad){
+
+		
+		this.especialidad = especialidad;
+
+	}
+
+//get especialidad
+
+	public int getEspecialidad(){
+
+		return especialidad;
+
+	}
+
+
+}//clase 
