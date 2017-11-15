eac348f279b335dc8a35fcf5583971ef9c2741cd
ItielBorja <33648137+ItielBorja@users.noreply.github.com>
2017-11-15 00:05:09 -0600

    Create Item.java

---
+++ Item.java
@@ -0,0 +1,80 @@
+public class Item{
+	
+	private String 	nombre;
+	private String 	tipo;
+	private int 	precio;
+	private String 	descrpcion;
+
+//Constructor
+
+	public Item(){
+
+
+	}
+
+//set nombre 
+
+	public void setNombre(String nombre){
+
+		this.nombre= nombre;
+
+	}
+
+//get nombre
+
+	public String getNombre(){
+
+		return nombre;
+
+	}
+
+//set tipo
+
+	public void setTipo(String tipo){
+
+		this.tipo= tipo;
+
+	}
+
+//get tipo
+
+	public String getTipo(){
+
+		return tipo;
+
+	}
+
+//set descripcion 
+
+	public void setDescripcion(String descripcion){
+
+		this.descripcion= descripcion;
+
+	}
+
+//get descripcion
+
+	public String getDescripcion(){
+
+		return descripcion;
+
+	}
+
+//set precio
+
+	public void setPrecio (int precio){
+
+		this.precio = precio;
+
+	}
+
+//get precio
+
+	public int getPrecio(){
+
+		return precio;
+
+	}
+
+
+}// clase
