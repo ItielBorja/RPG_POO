910e87494ad8ace25a5a0728e514fa4e0866d361
El-Ectron <33848027+El-Ectron@users.noreply.github.com>
2017-11-21 12:45:50 -0600

    Update VendedorDeArmadura.java

--- VendedorDeArmadura.java
+++ VendedorDeArmadura.java
@@ -1,3 +1,5 @@
 public class VendedorDeArmadura extends NPC{
-	
+	items[0]=new Armadura(1,50,200,"Chaleco");
+	items[1]=new Armadura(2,150,450,"Chaleco antibalas");
+	items[2]=new Armadura(3,400,750,"Chainmail");
 }
