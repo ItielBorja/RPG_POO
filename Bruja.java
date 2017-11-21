d360fdfbda8e0ae22f6f4f7cb1bf837b662788a4
El-Ectron <33848027+El-Ectron@users.noreply.github.com>
2017-11-21 13:07:15 -0600

    Update Bruja.java

--- Bruja.java
+++ Bruja.java
@@ -1,3 +1,7 @@
 public class Bruja extends Enemigo{
-	
+	public Bruja(int nivelDeJugador){
+		this.nivel=(int)(Math.random()*((nivelDeJugador+7)-(nivelDeJugador-7)))+(nivelDeJugador-7); //Puede ser 7 niveles menor o mayor que el jugador
+		this.xp=nivel*7;
+		this.dinero=nivel*7; //Experiencia y dinero que dan depende de su nivel;
+	}
 }
