14019647eb02d854c9b09c1fb868e67e7e27c533
El-Ectron <33848027+El-Ectron@users.noreply.github.com>
2017-11-21 12:41:17 -0600

    Update VendedorDeArmas.java

--- VendedorDeArmas.java
+++ VendedorDeArmas.java
@@ -1,5 +1,5 @@
 public class VendedorDeArmas extends NPC{
-	items[0]=new Arma(1,10,5,100);
-	items[1]=new Arma(2,25,10,500);
-	items[2]=new Arma(3,50,10,1200);
+	items[0]=new Arma(1,10,5,100,"Daga");
+	items[1]=new Arma(2,25,10,500,"Espada");
+	items[2]=new Arma(3,50,10,1200,"Espadón");
 }
