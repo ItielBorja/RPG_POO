996eaf5412eb9022b822b650a354390d042596e3
El-Ectron <33848027+El-Ectron@users.noreply.github.com>
2017-11-21 12:54:02 -0600

    Update Enemigo.java

--- Enemigo.java
+++ Enemigo.java
@@ -1,3 +1,16 @@
 public class Enemigo extends Personaje{
-	
+	private int dinero;
+	private int xp;
+	public int getXp(){
+		return xp;
+	}
+	public void setXp(int xp){
+		this.xp=xp;
+	}
+	public int getDinero(){
+		return dinero;
+	}
+	public void setDinero(int dinero){
+		this.dinero=dinero;
+	}
 }
