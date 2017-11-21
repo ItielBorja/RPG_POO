0cc19c7dc78dfdfb28b15541ee6027a1651df175
El-Ectron <33848027+El-Ectron@users.noreply.github.com>
2017-11-21 13:06:45 -0600

    Update Troll.java

--- Troll.java
+++ Troll.java
@@ -1,3 +1,7 @@
 public class Troll extends Enemigo{
-	
+	public Troll(int nivelDeJugador){
+		this.nivel=(int)(Math.random()*((nivelDeJugador+5)-(nivelDeJugador-5)))+(nivelDeJugador-5); //Puede ser 5 niveles menor o mayor que el jugador
+		this.xp=nivel*5;
+		this.dinero=nivel*5; //Experiencia y dinero que dan depende de su nivel;
+	}
 }
